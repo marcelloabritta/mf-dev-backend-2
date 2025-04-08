922be112e73708570648c54d5eef29de95f0aa87
marcelloabritta <abrittamarcello@gmail.com>
2025-04-08 15:45:10 -0300

    Configuração do banco de dados e da tabela veiculo

--- mf-dev-backend-2/Program.cs
+++ mf-dev-backend-2/Program.cs
@@ -1,3 +1,6 @@
+using mf_dev_backend_2.Models;
+using Microsoft.EntityFrameworkCore;
+
 namespace mf_dev_backend_2
 {
     public class Program
@@ -10,6 +13,9 @@ namespace mf_dev_backend_2
             builder.Services.AddControllersWithViews();
 
             builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
